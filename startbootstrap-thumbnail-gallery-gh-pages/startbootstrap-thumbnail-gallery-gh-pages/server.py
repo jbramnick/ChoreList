2bcef33029d94d72bc425c2cbf1e906564228ee7
Michael Bloecker <michaeljbloecker@gmail.com>
2017-10-28 20:23:55 +0000

    change index for loop

--- startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
+++ startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    thing = ['1', '2', '3', '4', '5']
+    return render_template('index.html', things = thing)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
-    
