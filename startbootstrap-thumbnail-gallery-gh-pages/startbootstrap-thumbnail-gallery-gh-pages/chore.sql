dc6d41642bb39e657f135292259f8554fd15fc67
Shawn McElroy <mac87eod@gmail.com>
2017-11-04 19:24:53 +0000

    Added admin data to admin database

--- startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/chore.sql
+++ startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/chore.sql
@@ -50,8 +50,10 @@ CREATE TABLE groups (
     
     /*test data for databases*/
 INSERT INTO usernames (username ) VALUES ('Brendon');
+INSERT INTO admin (id,name,group_id) VALUES (1,'Brendon',1); 
 INSERT INTO pass (id,password) VALUES (1,'abc1');
 INSERT INTO usernames (username ) VALUES ('Jesse');
+INSERT INTO admin (id,name,group_id) VALUES (2,'Jesse',1);
 INSERT INTO pass (id,password) VALUES (2,'abc2');
 INSERT INTO chore (name,description,rewardval, claimed,user_id,group_id) VALUES ('chore1','the first chore',5,false,1,1);
 INSERT INTO groups (name,admin_id) VALUES ('group1',1);
@@ -59,8 +61,10 @@ INSERT INTO users (id,name,points,group_id) VALUES (1,'bob',0,1);
 INSERT INTO reward (id,name,description,cost,stock,group_id) VALUES (1,'MONEY','Cash',5,1,1);
 
 INSERT INTO usernames (username ) VALUES ('Alice');
+INSERT INTO admin (id,name,group_id) VALUES (3,'Alice',2);
 INSERT INTO pass (id,password) VALUES (3,'abc3');
 INSERT INTO usernames (username ) VALUES ('Joe');
+INSERT INTO admin (id,name,group_id) VALUES (4,'Joe',2);
 INSERT INTO pass (id,password) VALUES (4,'abc4');
 INSERT INTO chore (name,description,rewardval, claimed,user_id,group_id) VALUES ('Sweep','Sweep the living room clean',10,false,2,2);
 INSERT INTO groups (name,admin_id) VALUES ('group2',2);
