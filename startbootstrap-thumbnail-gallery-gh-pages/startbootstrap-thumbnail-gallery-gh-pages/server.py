1cb135504bb4145c4d7d4a6fc1d730a5f7387cb7
jbramnick <jbramnick412@gmail.com>
2017-11-11 14:01:37 -0500

    Login and registration done and improved

--- startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
+++ startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import psycopg2.extras
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session,redirect 
 from flask_bootstrap import Bootstrap
 from config import *
 import data_postgres as pg
@@ -11,11 +11,15 @@ app.secret_key = os.urandom(24).encode('hex')
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+	if request.args["failed"]:
+		return render_template('login.html',failed=request.args["failed"])
+	return render_template('login.html')
+@app.route('/register')
+def register():
+	return render_template('login.html',disabled="disabled")
 @app.route('/about')
 def about():
 	return render_template('about.html')
-    
 @app.route('/index',methods=['POST'])
 def index():
 	if request.form['submit']=='Login':
@@ -26,17 +30,14 @@ def index():
 				session['Password'] = request.form['Password']
 				#session['id'] = results[0][2]
 				thing=[1,2,3,4,5,6,7]
-				user=request.form['Username']
-				passwd=request.form['Password']
 				#put database queries here
 				return render_template('index.html',things=thing,username=user)
+			else:
+				return redirect("/?failed=True")
 		except:
-			pass
-		return render_template('/login.html', failed = True)
+			return render_template("login.html", failed=True)
 	elif request.form['submit']=='Register':
-		render_template('/login.html',disabled="disabled")
-		print("hello")
-		return render_template('index.html') 
+		return redirect("/register")
 		
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
