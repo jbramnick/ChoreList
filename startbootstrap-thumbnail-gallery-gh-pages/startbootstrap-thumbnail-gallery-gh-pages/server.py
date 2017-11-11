6b130dfe7f8b339acf94af90855ae362820409d7
jbramnick <jbramnick412@gmail.com>
2017-11-11 15:45:59 -0500

    Completed some better authentication using redirects

--- startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
+++ startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
@@ -8,36 +8,56 @@ import data_postgres as pg
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24).encode('hex')
-
-@app.route('/')
-def home():
-	if request.args["failed"]:
-		return render_template('login.html',failed=request.args["failed"])
-	return render_template('login.html')
-@app.route('/register')
-def register():
-	return render_template('login.html',disabled="disabled")
-@app.route('/about')
-def about():
-	return render_template('about.html')
-@app.route('/index',methods=['POST'])
-def index():
+@app.route('/auth',methods=['POST'])
+def authorization():
 	if request.form['submit']=='Login':
 		try:
 			results = pg.get_user(request.form['Username'], request.form['Password'])
+			#LINE BELOW IS FOR UI DEVELOPMENT TESTING ONLY REMOVE THIS LINE FOR DEPLOYMENT
+			results=True
 			if results:
 				session['Username'] = request.form['Username']
 				session['Password'] = request.form['Password']
-				#session['id'] = results[0][2]
-				thing=[1,2,3,4,5,6,7]
-				#put database queries here
-				return render_template('index.html',things=thing,username=user)
+				return redirect("/index")
 			else:
 				return redirect("/?failed=True")
 		except:
-			return render_template("login.html", failed=True)
+			return redirect("/?failed=True")
 	elif request.form['submit']=='Register':
 		return redirect("/register")
-		
+def auth():
+	try:
+		if not session['Username']:
+			return False	
+		else:
+			return True
+	except:
+		return False
+@app.route('/')
+def home():
+	if not request.args:
+		return render_template('login.html', login=True)	
+	elif request.args["failed"]:
+		return render_template('login.html',failed=request.args["failed"],login=True)
+@app.route('/register')
+def register():
+	return render_template('login.html',disabled="disabled",login=True)
+@app.route('/about')
+def about():
+	if not auth():
+		return redirect("/")
+	return render_template('about.html',username=session['Username'],aboutPage="active")
+@app.route('/rewards')
+def rewards():
+	if not auth():
+		return redirect("/")
+	return render_template('rewards.html',rewardsPage="active",username=session['Username'])
+@app.route('/index')
+def index():
+	if not auth():
+		return redirect("/")
+	thing=[1,2,3,4,5,6,7]
+	#should prolly put database stuff here
+	return render_template('index.html',username=session['Username'],chorePage="active",things=thing)
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
