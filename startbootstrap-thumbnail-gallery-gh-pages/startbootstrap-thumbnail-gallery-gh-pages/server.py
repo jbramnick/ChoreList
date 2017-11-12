c242dd44e138f8338549d14dfa558b8e76310aef
jim Bramnick <jbramnick412@gmail.com>
2017-11-12 03:37:01 -0500

    Got pretty far with profile page finish later

--- startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
+++ startbootstrap-thumbnail-gallery-gh-pages/startbootstrap-thumbnail-gallery-gh-pages/server.py
@@ -11,7 +11,7 @@ app.secret_key = os.urandom(24).encode('hex')
 #call this method before every user page
 def auth():
 	try:
-		if not session['Username']:
+		if not session:
 			return False	
 		else:
 			return True
@@ -29,7 +29,10 @@ def authorization():
 			results=True
 		#END UI TESTING LINES
 		if results:
+			#DATABASE
+			points=20
 			session['Username'] = request.form['Username']
+			session['Points'] = points
 			return redirect("/"+page)
 		else:
 			return redirect("/?failed=True&page="+page)
@@ -37,16 +40,35 @@ def authorization():
 		return redirect("/?failed=True&page="+page)
 @app.route('/registerLog',methods=['POST'])
 def registerLog():
+	#DATABASE
 	#do database stuff here
 	try: 
 		#insert user into database
+		#DATABASE
 		session['Username']=request.form['Username']
+		session['Points']=20
 		if(request.form['Password']==request.form['ConfirmPassword']):
 			return redirect("/index")
 		else:
 			return redirect("/register?failed=True")
 	except:
 		return redirect("/register?failed=True")
+@app.route('/choreLog')
+def choreLog():
+	#DATABASE
+	#remove chore from database
+	return redirect("/chores?choreLog=True")
+@app.route('/rewardLog')
+def rewardLog():
+	#DATABASE
+	#remove a rewards stock from database
+	#also change the session['Points'] value 
+	#Change it for the user
+	return redirect("/rewards?rewardLog=True")
+@app.route('/profileDelta',methods=['POST'])
+def profileDelta():
+	#DATABASE
+	return redirect("/profile")
 @app.route('/')
 def home():
 	if not request.args:
@@ -62,11 +84,15 @@ def register():
 def about():
 	if not auth():
 		return redirect("/?page=about&failed=False")
-	return render_template('about.html',username=session['Username'],aboutPage="active")
+	return render_template('about.html',\
+	username=session['Username'],\
+	aboutPage="active",\
+	points=session['Points'])
 @app.route('/rewards')
 def rewards():
 	if not auth():
 		return redirect("/?page=rewards&failed=False")
+	#DATABASE
 	thing=[[1,2,3],[4,5,6]]
 	try:
 		if not request.args["rewardLog"]:
@@ -75,13 +101,18 @@ def rewards():
 			rewardLog=request.args["rewardLog"]
 	except:
 		rewardLog=None
-	return render_template('rewards.html',rewardsPage="active",username=session['Username'],things=thing,rewardLog=rewardLog)
+	return render_template('rewards.html',\
+	rewardsPage="active",\
+	username=session['Username'],\
+	things=thing,\
+	rewardLog=rewardLog,\
+	points=session['Points'])
 @app.route('/chores')
 def index():
 	if not auth():
 		return redirect("/?page=chores&failed=False")
 	thing=[[1,2,3],[4,5,6]]
-	#should prolly put database stuff here
+	#DATABASE
 	try:
 		if not request.args["choreLog"]:
 			choreLog=None
@@ -89,19 +120,20 @@ def index():
 			choreLog=request.args["choreLog"]
 	except:
 		choreLog=None
-	return render_template('chores.html',username=session['Username'],choresPage="active",things=thing,choreLog=choreLog)
+	return render_template('chores.html',\
+	username=session['Username'],\
+	choresPage="active",\
+	things=thing,\
+	choreLog=choreLog,\
+	points=session['Points'])
 @app.route('/profile')
 def profile():
+	#DATABASE
 	if not auth():
 		return redirect("/?page=profile&failed=False")
-	return render_template('profile.html',username=session['Username'],profilePage="active")
-@app.route('/choreLog')
-def choreLog():
-	#remove chore from database
-	return redirect("/chores?choreLog=True")
-@app.route('/rewardLog')
-def rewardLog():
-	#remove a rewards stock from database
-	return redirect("/rewards?rewardLog=True")
+	return render_template('profile.html',\
+	username=session['Username'],\
+	profilePage="active",\
+	points=session['Points'])
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
